09178c3bbb4b7cb13d67318297cdcac2c7cc0bd0
Lisa White <whitelisab@gmail.com>
2020-07-14 20:10:40 -0600

    Updated data generation and seeding for 10 mill

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,12 +1,9 @@
 const path = require('path');
 const db = require('./index.js');
 
-let ts = Date.now();
-let dateObj = new Date(ts);
-
 // seed chars table
 const seedCharsTable = () => {
-  console.log('started seeding characteristics', dateObj.toTimeString(), ts);
+  console.log('started seeding characteristics', new Date().toUTCString());
   const pathToCSV = path.join(__dirname, 'chars.csv');
   const delimiter = ',';
   const sqlString = `COPY characteristics FROM '${pathToCSV}' DELIMITER '${delimiter}' CSV HEADER`;
@@ -15,7 +12,7 @@ const seedCharsTable = () => {
 
 // seed reviews table
 const seedReviewsTable = () => {
-  console.log('started seeding reviews', dateObj.toTimeString(), ts);
+  console.log('started seeding reviews', new Date().toUTCString());
   const pathToCSV = path.join(__dirname, 'reviews.csv');
   const delimiter = ',';
   const sqlString = `COPY reviews(rating,summary,recommend,response,body,date,reviewer_name,reviewer_email,verified,helpfulness,helpfulness_no,product_id,reported) FROM '${pathToCSV}' DELIMITER '${delimiter}' CSV HEADER`;
@@ -24,7 +21,7 @@ const seedReviewsTable = () => {
 
 // // seed join table
 const seedJoinTable = () => {
-  console.log('started seeding join table', dateObj.toTimeString(), ts);
+  console.log('started seeding join table', new Date().toUTCString());
   const pathToCSV = path.join(__dirname, 'joinChar.csv');
   const delimiter = ',';
   const sqlString = `COPY reviews_characteristics(review_id,char_id,rating) FROM '${pathToCSV}' DELIMITER '${delimiter}' CSV HEADER`;
@@ -33,21 +30,21 @@ const seedJoinTable = () => {
 
 // seed photos table
 const seedPhotosTable = () => {
-  console.log('started seeding photos', dateObj.toTimeString(), ts);
-  const pathToCSV = path.join(__dirname, 'photos.csv');
+  console.log('started seeding photos', new Date().toUTCString());
+  const pathToCSV = path.join(__dirname, 'reviews_photos.csv');
   const delimiter = ',';
   const sqlString = `COPY photos FROM '${pathToCSV}' DELIMITER '${delimiter}' CSV HEADER`;
   return db.query(sqlString);
 };
 
 // invoke seeding functions
-console.log('begin invoking functions', dateObj.toTimeString(), ts);
+console.log('begin invoking functions', new Date().toUTCString());
 seedCharsTable()
-  .then(() => console.log('Imported characteristics table', dateObj.toTimeString(), ts))
+  .then(() => console.log('Imported characteristics table', new Date().toUTCString()))
   .then(seedReviewsTable)
-  .then(() => console.log('Imported reviews table', dateObj.toTimeString(), ts))
+  .then(() => console.log('Imported reviews table', new Date().toUTCString()))
   .then(seedJoinTable)
-  .then(() => console.log('Imported join table', dateObj.toTimeString(), ts))
+  .then(() => console.log('Imported join table', new Date().toUTCString()))
   .then(seedPhotosTable)
-  .then(() => console.log('Imported photos table', dateObj.toTimeString(), ts))
+  .then(() => console.log('Imported photos table', new Date().toUTCString()))
   .catch(console.log);
